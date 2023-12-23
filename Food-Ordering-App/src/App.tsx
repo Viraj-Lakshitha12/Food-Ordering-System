a177a0ecf64144d86e925ff53d6d7d857dc52659
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-23 21:48:11 +0530

    interduce tailwind css

--- Food-Ordering-App/src/App.tsx
+++ Food-Ordering-App/src/App.tsx
@@ -2,10 +2,10 @@ import './App.css'
 
 function App() {
     return (
-        <div>
-            Hello
-        </div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     )
-};
+}
 
 export default App
