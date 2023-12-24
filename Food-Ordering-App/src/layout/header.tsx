5e4909107da96d84c1e46bafa48912c496b7fcef
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-24 09:17:25 +0530

    update header section

--- Food-Ordering-App/src/layout/header.tsx
+++ Food-Ordering-App/src/layout/header.tsx
@@ -2,15 +2,15 @@
 export default function Header(){
 
         return (
-            <header>
-                <strong className={'text-red-500 font-bold'}>PIZZA</strong>
+            <header className={'flex items-center justify-between'}>
+                <strong className={'text-4xl text-red-600 font-bold p-4'}>PIZZA</strong>
                 <nav>
-                    <ul className={'inline'}>
+                    <ul className={'flex items-center mx-5 text-gray-500 gap-8 font-semibold'}>
                         <li>Home</li>
                         <li>Menu</li>
                         <li>About</li>
                         <li>Contact</li>
-                        <li>Login</li>
+                        <li className={'bg-red-600 text-white px-4 py-2 rounded-full'}>Login</li>
                     </ul>
                 </nav>
             </header>
