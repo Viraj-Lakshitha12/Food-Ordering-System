7a67072043729d6e3a19bbf4324a4512994dea77
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-24 17:49:45 +0530

    update contact section

--- Food-Ordering-App/src/views/contact.tsx
+++ Food-Ordering-App/src/views/contact.tsx
@@ -1,10 +1,13 @@
-export default function Contact(){
-    return(
-        <section className={'mt-10'}>
+export default function Contact() {
+    return (
+        <section className="mt-10">
             <div className="text-center text-gray-500 font-semibold text-xl">
                 <h3>DON'T WASTE TIME</h3>
                 <h2 className="font-bold text-red-600 text-4xl italic">Contact</h2>
             </div>
+            <div className="flex items-center justify-center mt-6">
+                <a className="text-black font-bold text-4xl" href={'tel:0111234567'}>011-1234567</a>
+            </div>
         </section>
-    )
+    );
 }
