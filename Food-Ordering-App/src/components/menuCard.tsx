4b47d09fd605dfc02900258a9a978288c454ffb1
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-25 23:17:55 +0530

    update content

--- Food-Ordering-App/src/components/menuCard.tsx
+++ Food-Ordering-App/src/components/menuCard.tsx
@@ -1,13 +1,40 @@
+interface Data {
+    name: string;
+    content: string;
+}
+
+const data: Data[] = [
+    {
+        name: 'Pepperoni pizza',
+        content: 'A classic favorite with pepperoni and savory flavors. Perfect for pizza enthusiasts!',
+    },
+    {
+        name: 'Peporoni Pizza',
+        content: 'lorem',
+    },
+    {
+        name: 'Peporoni Pizza',
+        content: 'lorem',
+    },
+];
+
 export default function MenuCard() {
     return (
-        <div className="hover:shadow-black hover:bg-white transition-all hover:shadow-2xl col-span-1 mx-auto bg-gray-200 rounded-lg text-center p-2 flex flex-col items-center max-w-[300px]">
-            <img className="w-[280px] h-[280px] object-cover rounded-3xl"
-                 src="src/assets/images/peparoniPizza.png" alt="pizza"/>
-            <h1 className="font-semibold text-xl">Pepperoni pizza</h1>
-            <p className=""> A classic favorite with pepperoni and savory flavors. Perfect for pizza enthusiasts!</p>
-            <button className="rounded-full bg-red-600 px-8 py-2 m-3 text-center text-white">Add to cart
-            </button>
-        </div>
+        <>
+            {data.map((pizza: Data, index: number) => (
+                <div
+                    key={index}
+                    className="hover:shadow-black hover:bg-white transition-all hover:shadow-2xl col-span-1 mx-auto bg-gray-200 rounded-lg text-center p-2 flex flex-col items-center max-w-[300px]"
+                >
+                    <img className="w-[280px] h-[280px] object-cover rounded-3xl"
+                         src="src/assets/images/peparoniPizza.png" alt="pizza"/>
 
-    )
+                    <h1 className="font-semibold text-xl">{pizza.name}</h1>
+                    <p className="text-gray-500">{pizza.content}</p>
+                    <button className="rounded-full bg-red-600 px-8 py-2 m-3 text-center text-white">Add to cart
+                    </button>
+                </div>
+            ))}
+        </>
+    );
 }
