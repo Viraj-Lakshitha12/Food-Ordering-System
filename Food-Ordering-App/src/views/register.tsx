dfa8483db664faa68710b7e9ef143aa2e9df4805
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-30 21:16:05 +0530

    update content

--- Food-Ordering-App/src/views/register.tsx
+++ Food-Ordering-App/src/views/register.tsx
@@ -12,7 +12,7 @@ const RegisterForm = () => {
                             type="email"
                             id="email"
                             name="email"
-                            className="mt-1 p-3 w-full rounded-md bg-gray-300 text-black font-semibold"
+                            className="mt-1 p-3 w-full rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800"
                             placeholder="example@example.com"
                         />
                     </div>
@@ -24,17 +24,35 @@ const RegisterForm = () => {
                             type="password"
                             id="password"
                             name="password"
-                            className="mt-1 p-3 w-full rounded-md bg-gray-300 text-black font-semibold"
-                            placeholder="********"
-                        />
+                            className="mt-1 p-3 w-full rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800"
+                            placeholder="********"/>
                     </div>
+
                     <button
-                        type="submit"
-                        className="w-full bg-red-600 text-white p-3 rounded-md hover:bg-red-700 transition duration-300"
-                    >
-                        Register
+                        className="relative flex w-full bg-red-600 p-3 rounded-md transition duration-300
+                         items-center justify-center overflow-hidden
+                         font-medium text-white shadow-2xl  before:absolute before:inset-0 before:border-0
+                          before:border-white before:duration-100 before:ease-linear hover:bg-white hover:text-red-600 hover:shadow-
+                          blue-600 hover:before:border-[25px]">
+                        <span className="relative z-10">Register</span>
                     </button>
+
+                    {/*<button*/}
+                    {/*    type="submit"*/}
+                    {/*    className="w-full bg-red-600 text-white p-3 rounded-md hover:bg-red-700 transition duration-300">*/}
+                    {/*    Register*/}
+                    {/*</button>*/}
+
                 </form>
+                <div className={'text-black text-center p-2 font-semibold mt-2'}>
+                    or login with provider
+                </div>
+
+                <button
+                    className={'flex gap-2 items-center justify-center text-gray-900 my-4 w-full py-2 text-center border-2 border-black rounded-md'}>
+                    <img src={'src/assets/images/google-icon.png'} alt={'google'} width={'32'} height={'32'}/>Login
+                    with google
+                </button>
             </div>
         </div>
     );
