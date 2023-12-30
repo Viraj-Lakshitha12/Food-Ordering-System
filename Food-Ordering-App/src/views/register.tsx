a30f6bbdd3f3251dbbfd25c84b52b48c6a197200
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-30 17:59:49 +0530

    update register page

--- Food-Ordering-App/src/views/register.tsx
+++ Food-Ordering-App/src/views/register.tsx
@@ -1,11 +1,43 @@
-export default function Register() {
+const RegisterForm = () => {
     return (
-        <section>
-            <h1 className={'font-semibold text-red-600 text-4xl text-center'}> Register</h1>
-            <form className={'rounded-xl'}>
-            <input className={'border-gray-300 bg-gray-200 p-3 rounded-xl min-w-full m-4 '} type={"email"} placeholder={'Email'}/>
-            <input className={'border-gray-300 bg-gray-200 p-3 rounded-xl m-4 '} type={"password"} placeholder={'Password'}/>
-            </form>
-        </section>
-    )
-}
+        <div className="flex flex-col justify-center items-center  bg-center h-screen w-screen">
+            <div className="p-8 rounded-md text-white w-96">
+                <h1 className="text-3xl font-semibold mb-6 text-red-600">Register</h1>
+                <form className="space-y-4">
+                    <div>
+                        <label htmlFor="email" className="block text-sm  text-gray-500 font-bold">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="mt-1 p-3 w-full rounded-md bg-gray-300 text-black font-semibold"
+                            placeholder="example@example.com"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block text-sm  text-gray-500 font-bold">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="mt-1 p-3 w-full rounded-md bg-gray-300 text-black font-semibold"
+                            placeholder="********"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-red-600 text-white p-3 rounded-md hover:bg-red-700 transition duration-300"
+                    >
+                        Register
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterForm;
