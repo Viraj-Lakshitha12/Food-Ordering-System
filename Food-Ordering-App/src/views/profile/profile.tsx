97709c23c890f0595171ad16900c9b03920a310b
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-03 21:25:11 +0530

    updated

---
+++ Food-Ordering-App/src/views/profile/profile.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Profile() {
+
+
+
+    return (
+
+        <section className="my-10">
+            <div className="text-center text-red-500 font-semibold text-4xl">
+                <h3>Profile</h3>
+            </div>
+        </section>
+    )
+}
