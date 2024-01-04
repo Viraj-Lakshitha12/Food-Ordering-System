08474969a8b5d901a88903475addd71cf3b8d0fd
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-04 20:00:14 +0530

    create post method and catch email and password

--- Food-Ordering-backend/src/index.ts
+++ Food-Ordering-backend/src/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import * as process from "process";
 import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 
 
 let app = express();
+app.use(bodyParser.json());
+
 mongoose.connect(process.env.MONGO_URL as string);
 
 const db = mongoose.connection;
@@ -16,12 +20,27 @@ db.on('error', (error) => {
 
 db.on('open', () => {
     console.log("DB OK");
-})
+});
+
+// app.use(cors({origin: '*'}))
 
 app.listen(8080, () => {
     console.log("Server started 8080");
 });
 
+app.use(cors({origin:'*'}))
+
 app.get('/test', (req: express.Request, res: express.Response) => {
     res.status(200).send("working");
-})
+});
+
+app.post('/api/register', (req: express.Request, res: express.Response) => {
+    const { email, password } = req.body;
+
+    // Do something with the data
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    // Send a response
+    res.status(200).send('Received JSON data successfully');
+});
