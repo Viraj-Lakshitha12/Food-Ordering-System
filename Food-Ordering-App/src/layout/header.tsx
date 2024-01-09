895fbd24f6d0ac882be1e62896f03edb64e6dd08
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-09 21:22:39 +0530

    update hearder section

--- Food-Ordering-App/src/layout/header.tsx
+++ Food-Ordering-App/src/layout/header.tsx
@@ -1,20 +1,30 @@
 import {Link} from "react-router-dom";
+import Cookies from "js-cookie";
 
-export default function Header(){
+export default function Header() {
+    let token = Cookies.get('token');
 
-        return (
-            <header className={'flex items-center justify-between'}>
-                <strong className={'text-4xl text-red-600 font-bold p-4'}>PIZZA</strong>
-                <nav>
-                    <ul className={'flex items-center mx-5 text-gray-500 text-lg gap-10 font-semibold'}>
-                        <li><Link to={'/'}>Home</Link></li>
-                        <li><Link to={'/menu'}>Menu</Link></li>
-                        <li><Link to={'/about'}>About</Link></li>
-                        <li><Link to={'/contact'}>Contact</Link></li>
-                        <li className={''} ><Link to={'/login'}>Login</Link></li>
-                        <li className={'bg-red-600 text-white px-4 py-2 rounded-full'}><Link to={'/register'}>Register</Link> </li>
-                    </ul>
-                </nav>
-            </header>
-        )
+    return (
+        <header className={'flex items-center justify-between'}>
+            <strong className={'text-4xl text-red-600 font-bold p-4'}>PIZZA</strong>
+            <nav>
+                <ul className={'flex items-center mx-5 text-gray-500 text-lg gap-10 font-semibold'}>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/menu'}>Menu</Link></li>
+                    <li><Link to={'/about'}>About</Link></li>
+                    <li><Link to={'/contact'}>Contact</Link></li>
+                    {token ? (
+                        <>
+                            <li className={''}><Link to={'/login'}>Login</Link></li>
+                            <li className={'bg-red-600 text-white px-4 py-2 rounded-full'}><Link
+                                to={'/register'}>Register</Link></li>
+                        </>
+                    ) : (
+                        <li className={'bg-red-600 text-white px-4 py-2 rounded-full'}>Logout</li>
+
+                    )}
+                </ul>
+            </nav>
+        </header>
+    );
 }
