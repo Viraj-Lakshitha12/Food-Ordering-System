2365b0cd5f5256de7713b4da777d97d112b0d32b
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-10 16:26:31 +0530

    images update  profile tsx

--- Food-Ordering-App/src/views/profile/profile.tsx
+++ Food-Ordering-App/src/views/profile/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 function UserProfile() {
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
@@ -20,14 +20,14 @@ function UserProfile() {
     return (
         <section className="my-10">
             <div className="flex flex-col items-center">
-                <div className="w-full max-w-md">
-                    <div className="">
+                <div className=" max-w-md">
+                    <div className="w-full max-w-md">
                         {selectedImage ? (
                             <img
                                 src={selectedImage}
                                 alt="User Profile"
-                                className="rounded-2xl mx-auto my-4"
-                                style={{ maxWidth: '200px', maxHeight: '200px' }}
+                                className=" rounded-2xl mx-auto my-4"
+                                style={{maxWidth: '200px', maxHeight: '200px'}}
                             />
                         ) : (
                             <div className="text-center my-4 font-bold mx-auto">
@@ -36,14 +36,20 @@ function UserProfile() {
                         )}
                         <label
                             htmlFor="imageInput"
-                            className="cursor-pointer border border-gray-800 text-center w-full block p-4"
-                        >
+                            className="cursor-pointer border border-gray-800 text-center max-w-xl block p-2">
                             Choose an image
-                            <input type="file" id="imageInput" accept="image/*" onChange={handleImageChange} className="hidden" />
+                            <input type="file" id="imageInput" accept="image/*" onChange={handleImageChange}
+                                   className="hidden"/>
                         </label>
                     </div>
-                    <div className="mt-4">
-                        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-full" onClick={handleProfileUpdate}>
+                    <div className={'flex flex-col items-center'}>
+                        <input className={'mt-1 p-2 max-w-4xl rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center'} type={'text'} placeholder={"First and last name"}/>
+                    </div>
+
+
+                    <div className="mt-4  flex flex-col items-center">
+                        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-full"
+                                onClick={handleProfileUpdate}>
                             Update Profile
                         </button>
                     </div>
