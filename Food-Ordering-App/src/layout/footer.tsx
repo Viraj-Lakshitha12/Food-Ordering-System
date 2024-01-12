cc1559767c0e4cb0dfbb82848ce8d9ab27327935
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-12 13:02:49 +0530

    update footer

--- Food-Ordering-App/src/layout/footer.tsx
+++ Food-Ordering-App/src/layout/footer.tsx
@@ -2,12 +2,12 @@ export default function Footer(){
     return(
         <div>
             <footer className={'bg-teal-950 text-white p-5'}>
-                <img src="src/assets/images/pizza.png" title="logo" alt="logo" className={'w-[100px]'}/>
+                <img src={"src/assets/images/pizza.png"} title="logo" alt="logo" className={'w-[100px]'}/>
                 <ul className={'mt-5'}>
                     <li>123D, Flower Rd, Colombo</li>
                     <li>+94 76 722 3131</li>
                     <li>+94 38 722 3131</li>
-                    <li>info@blog.lk</li>
+                        <li>info@pizza.lk</li>
                 </ul>
                 <div className={'mt-5 text-center'}>Copyright © 2023 pizza LK</div>
             </footer>
