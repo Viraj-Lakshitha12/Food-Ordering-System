884fd5646ff4b19a0f3fd84b7b7f52cd5c19bc6d
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-13 20:11:08 +0530

    load the main page after set admin or not

--- Food-Ordering-App/src/views/home.tsx
+++ Food-Ordering-App/src/views/home.tsx
@@ -1,16 +1,41 @@
-
 import Hero from "../layout/hero.tsx";
 import HomeMenu from "./homeMenu.tsx";
 import About from "./about.tsx";
 import Contact from "./contact.tsx";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 
-export default function Home(){
-    return(
+const fetchData = async () => {
+    const user = Cookies.get('user');
+    if (user) {
+        try {
+            const email = user;
+            const response = await axios.get(`http://localhost:8080/api/user/getUserDetailsByEmail/${email}`);
+            const userDetailData = response.data.data;
+
+            console.log('admin', userDetailData.admin);
+            Cookies.set('admin', userDetailData.admin);
+
+        } catch (error) {
+            console.error('Error fetching user details:', error);
+        }
+    }
+};
+
+const Home = () => {
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
         <div>
             <Hero />
             <HomeMenu />
             <About />
             <Contact />
         </div>
-    )
-}
+    );
+};
+
+export default Home;
--- Food-Ordering-App/src/views/login.tsx
+++ Food-Ordering-App/src/views/login.tsx
@@ -1,5 +1,5 @@
 import {Link, useNavigate} from "react-router-dom";
-import React, {useState} from "react";
+import React, { useState} from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 import Cookies from 'js-cookie';
@@ -53,6 +53,8 @@ export default function Login() {
             });
         }
     };
+
+
 
 
     return (
