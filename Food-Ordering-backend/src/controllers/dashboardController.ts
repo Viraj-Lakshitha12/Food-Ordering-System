f221380c81f368f8d55b9fe4c1db2e44d9c39b17
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-14 23:58:32 +0530

    create get all Categories

--- Food-Ordering-backend/src/controllers/dashboardController.ts
+++ Food-Ordering-backend/src/controllers/dashboardController.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {CategoryModel} from "../models/category";
 import CustomResponse from "../util/customResponse";
 
+//save Category
 export const saveCategory = async (req: express.Request, res: any) => {
     let req_body = req.body;
     console.log(req_body)
@@ -10,4 +11,14 @@ export const saveCategory = async (req: express.Request, res: any) => {
     }).catch(error => {
         res.status(500).send(new CustomResponse(500, "something went wrong", error));
     });
+}
+
+// get Category
+export const getAllCategories = async (req: express.Request, res: any) => {
+     await CategoryModel.find().then(r => {
+        res.status(200).send(new CustomResponse(200, "find all categories", r))
+    }).catch(error => {
+        res.status(500).send(new CustomResponse(500, "something went wrong", error));
+    });
+
 }
--- Food-Ordering-backend/src/routes/dashboard.ts
+++ Food-Ordering-backend/src/routes/dashboard.ts
@@ -1,10 +1,13 @@
 import express, {Router} from "express";
-import {saveCategory} from "../controllers/dashboardController";
+import {getAllCategories, saveCategory} from "../controllers/dashboardController";
 
 const route: Router = express.Router();
 
 
 //save category
 route.post('/saveCategory', saveCategory);
+
+//get all categories
+route.get('/getAllCategories', getAllCategories);
 
 export default route;
