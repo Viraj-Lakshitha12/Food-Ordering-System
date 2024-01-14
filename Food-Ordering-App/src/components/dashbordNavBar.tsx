b18d7ccbb25fe403f441c73b04f1775441fa34d0
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-14 09:31:00 +0530

    create dashboard navbar componanet

---
+++ Food-Ordering-App/src/components/dashbordNavBar.tsx
@@ -0,0 +1,27 @@
+import {Link} from "react-router-dom";
+
+export function DashbordNavBar() {
+    return (
+        <section>
+            <div className="flex items-center justify-center flex-col">
+                <div className="text-center">
+                    <strong className="text-4xl text-red-600  font-bold my-7">DASHBOARD</strong>
+                </div>
+                <nav>
+                    <ul className="flex mx-5 text-gray-500 text-lg gap-6 font-semibold my-7 ">
+                        <li className={'bg-gray-300 text-black rounded-2xl p-2 px-5'}>
+                            <Link to="/categories">Categories</Link>
+                        </li>
+                        <li className={'bg-gray-300 text-black rounded-2xl p-2 px-5'}>
+                            <Link to="/menuItems">Menu Items</Link>
+                        </li>
+                        <li className={'bg-gray-300 text-black rounded-2xl p-2 px-5'}>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </section>
+    );
+}
