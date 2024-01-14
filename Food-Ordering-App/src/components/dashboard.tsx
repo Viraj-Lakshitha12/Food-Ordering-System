1594bc038a4d7a206385ff50a34e5d2f2fe5d633
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-14 08:47:29 +0530

    update footer section

--- Food-Ordering-App/src/components/dashboard.tsx
+++ Food-Ordering-App/src/components/dashboard.tsx
@@ -1,7 +1,26 @@
+import {Link} from "react-router-dom";
+
 export function Dashboard() {
     return (
         <section>
-            <h1>dashboard</h1>
+            <div className="flex items-center justify-center flex-col">
+                <div className="text-center">
+                    <strong className="text-4xl text-red-600  font-bold my-7">DASHBOARD</strong>
+                </div>
+                <nav>
+                    <ul className="flex mx-5 text-gray-500 text-lg gap-10 font-semibold my-7">
+                        <li>
+                            <Link to="/categories">Categories</Link>
+                        </li>
+                        <li>
+                            <Link to="/menuItems">Menu Items</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </section>
     );
 }
--- Food-Ordering-App/src/layout/footer.tsx
+++ Food-Ordering-App/src/layout/footer.tsx
@@ -1,5 +1,5 @@
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <div>
             <footer className={'bg-teal-950 text-white p-5'}>
                 <img src={"src/assets/images/pizza.png"} title="logo" alt="logo" className={'w-[100px]'}/>
@@ -7,9 +7,9 @@ export default function Footer(){
                     <li>123D, Flower Rd, Colombo</li>
                     <li>+94 76 722 3131</li>
                     <li>+94 38 722 3131</li>
-                        <li>info@pizza.lk</li>
+                    <li>info@pizza.lk</li>
                 </ul>
-                <div className={'mt-5 text-center'}>Copyright © 2023 pizza LK</div>
+                <div className={'mt-5 text-center'}>Copyright © 2024 pizza LK</div>
             </footer>
         </div>
     )
