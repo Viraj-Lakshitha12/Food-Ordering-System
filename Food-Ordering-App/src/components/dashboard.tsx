b28acfd3d29fc61d846a1b684ed706b21d8ed147
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-14 08:52:00 +0530

    create dashboard nav bar

--- Food-Ordering-App/src/components/dashboard.tsx
+++ Food-Ordering-App/src/components/dashboard.tsx
@@ -8,14 +8,14 @@ export function Dashboard() {
                     <strong className="text-4xl text-red-600  font-bold my-7">DASHBOARD</strong>
                 </div>
                 <nav>
-                    <ul className="flex mx-5 text-gray-500 text-lg gap-10 font-semibold my-7">
-                        <li>
+                    <ul className="flex mx-5 text-gray-500 text-lg gap-6 font-semibold my-7 ">
+                        <li className={'bg-gray-300 text-black rounded-2xl p-2 px-5'}>
                             <Link to="/categories">Categories</Link>
                         </li>
-                        <li>
+                        <li className={'bg-gray-300 text-black rounded-2xl p-2 px-5'}>
                             <Link to="/menuItems">Menu Items</Link>
                         </li>
-                        <li>
+                        <li className={'bg-gray-300 text-black rounded-2xl p-2 px-5'}>
                             <Link to="/users">Users</Link>
                         </li>
                     </ul>
