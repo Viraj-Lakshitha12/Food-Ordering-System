91c7bf45270b690ead160be071b5cb1254920bcc
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-15 10:49:59 +0530

    create category update route

--- Food-Ordering-backend/src/routes/dashboard.ts
+++ Food-Ordering-backend/src/routes/dashboard.ts
@@ -1,11 +1,14 @@
 import express, {Router} from "express";
-import {getAllCategories, saveCategory} from "../controllers/dashboardController";
+import {getAllCategories, saveCategory, updateCategory} from "../controllers/dashboardController";
 
 const route: Router = express.Router();
 
 
 //save category
 route.post('/saveCategory', saveCategory);
+
+//update category
+route.put('/saveCategory', updateCategory);
 
 //get all categories
 route.get('/getAllCategories', getAllCategories);
