6f83f133d52724c1443237b93da6b3a8494b4041
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-15 21:11:16 +0530

    Check if admin is inside dashboard.txs

--- Food-Ordering-App/src/components/dashboard.tsx
+++ Food-Ordering-App/src/components/dashboard.tsx
@@ -1,7 +1,15 @@
-import {Link} from "react-router-dom";
 import {DashbordNavBar} from "./dashbordNavBar.tsx";
+import Cookies from "js-cookie";
 
 export function Dashboard() {
+    // @ts-ignore
+    const isAdmin = Cookies.get('admin') === 'true' || Cookies.get('admin') === true;
+
+    if (!isAdmin) {
+        alert("Access denied. You are not an admin.");
+        return null;
+    }
+
     return (
         <section>
             <DashbordNavBar/>
