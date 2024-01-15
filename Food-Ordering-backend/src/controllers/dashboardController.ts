bc151b7e6d3538141662181e30a7561ad9786fba
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-15 10:48:57 +0530

    create category update controller

--- Food-Ordering-backend/src/controllers/dashboardController.ts
+++ Food-Ordering-backend/src/controllers/dashboardController.ts
@@ -13,9 +13,30 @@ export const saveCategory = async (req: express.Request, res: any) => {
     });
 }
 
+//update category
+export const updateCategory = async (req: express.Request, res: any) => {
+    let req_body = req.body;
+    console.log(req_body)
+    let findOneCategory = await CategoryModel.findOne({_id: req_body._id});
+    if (findOneCategory) {
+        await CategoryModel.findOneAndUpdate({_id: req_body._id}, {
+            type: req_body.type
+        }).then(r => {
+            res.status(200).send(new CustomResponse(200, "update category", r));
+        }).catch(error => {
+            res.status(500).send(new CustomResponse(500, "something went wrong", error));
+        });
+    } else {
+        res.status(500).send(new CustomResponse(500, "cannot find category !"));
+    }
+
+
+}
+
+
 // get Category
 export const getAllCategories = async (req: express.Request, res: any) => {
-     await CategoryModel.find().then(r => {
+    await CategoryModel.find().then(r => {
         res.status(200).send(new CustomResponse(200, "find all categories", r))
     }).catch(error => {
         res.status(500).send(new CustomResponse(500, "something went wrong", error));
