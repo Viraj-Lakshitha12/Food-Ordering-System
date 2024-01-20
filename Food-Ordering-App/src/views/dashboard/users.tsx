6fbdbd2696d52e17ad04e905c4be2d842b4bc8fe
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-20 12:17:29 +0530

    create user section and get user details

--- Food-Ordering-App/src/views/dashboard/users.tsx
+++ Food-Ordering-App/src/views/dashboard/users.tsx
@@ -1,10 +1,46 @@
-import {Dashboard} from "../../components/dashboard.tsx";
+import React from 'react';
+import {Dashboard} from '../../components/dashboard.tsx';
+import axios from 'axios';
+
+interface UsersData {
+    userName: string;
+    email: string;
+    address: string;
+    postalCode: string;
+    city: string;
+    admin: boolean;
+    image?: File | null | string;
+}
 
 export function Users() {
+    const [users, setUsers] = React.useState<UsersData[]>([]);
+
+    React.useEffect(() => {
+        axios
+            .get('http://localhost:8080/api/user/getAllUserDetails')
+            .then((response) => {
+                setUsers(response.data.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <section>
             <Dashboard/>
-            <h1>Users</h1>
+            <div>
+                <h2 className={'text-center text-xl font-semibold'}>User List</h2>
+                {users.length > 0 ? (
+                    <div className={'bg-gray-200 max-w-xl mx-auto'}>
+                        {users.map((user: UsersData) => (
+                            <div className={'text-center'}>{user.userName}</div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No users available.</p>
+                )}
+            </div>
         </section>
     );
 }
