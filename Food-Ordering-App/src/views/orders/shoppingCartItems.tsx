dd1e79818e0eb772e76b9afc5bcecdcbba162562
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-22 22:51:33 +0530

    create checkout option

--- Food-Ordering-App/src/views/orders/shoppingCartItems.tsx
+++ Food-Ordering-App/src/views/orders/shoppingCartItems.tsx
@@ -1,10 +1,10 @@
-import { useCart } from '../dashboard/menu/CartContext.tsx';
+import {useCart} from '../dashboard/menu/CartContext.tsx';
 import Delete from '../../assets/icons/delete.tsx';
-import { Bounce, toast, ToastContainer } from 'react-toastify';
+import {Bounce, toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function ShoppingCartItems() {
-    const { cartItems, removeFromCart } = useCart();
+    const {cartItems, removeFromCart} = useCart();
 
     const notify = () => {
         toast('Product was removed');
@@ -15,17 +15,20 @@ export default function ShoppingCartItems() {
         notify();
     };
 
+    // Calculate total price
+    const total: number = cartItems.reduce((acc, item) => acc + item.price, 0);
+
     return (
-        <section className="mt-8">
-            <div className="text-center text-red-600 font-bold text-3xl">
+        <section className="mt-8 ml-8 mr-8">
+            <div className="text-center text-red-600 mb-8 font-bold text-3xl">
                 <h3>Cart</h3>
             </div>
             <div className="gap-4 grid grid-cols-2 pb-8 border-b">
                 <div>
-                    {cartItems?.map((item) => (
+                    {cartItems?.length === 0 ? 'No Product item Available' : cartItems?.map((item) => (
                         <div className="flex gap-16 mt-4 pb-2 items-center border-b" key={item.id}>
                             <div className="w-20">
-                                <img src={item.image} alt="pizza" width={100} height={100} />
+                                <img src={item.image} alt="pizza" width={100} height={100}/>
                             </div>
                             <div className="font-semibold text-lg grow">
                                 <p>{item.name}</p>
@@ -41,13 +44,22 @@ export default function ShoppingCartItems() {
                                     }}
                                     className="font-semibold shadow border-2 items-center px-4 py-2 bg-white rounded-xl"
                                 >
-                                    <Delete />
+                                    <Delete/>
                                 </button>
                             </div>
                         </div>
                     ))}
+                    <div className="text-right text-2xl mt-4 font-semibold">
+                        Total: ${total.toFixed(2)}
+                    </div>
                 </div>
-                <div>Right</div>
+                <div className={' p-8 bg-gray-200 rounded-xl'}>
+                    <h3 className={'text-xl font-semibold text-center'}>Checkout</h3>
+                    <form>
+                        <button type={'submit'} className={'w-full text-white bg-red-600 rounded-md py-4 px-24 text-xl'}>Pay
+                            ${total}</button>
+                    </form>
+                </div>
             </div>
             <ToastContainer
                 position="top-center"
