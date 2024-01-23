724a43433c2524e743bcf76529e259e4bff36387
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-23 19:07:06 +0530

    update order model

--- Food-Ordering-backend/src/models/order.ts
+++ Food-Ordering-backend/src/models/order.ts
@@ -1,12 +1,19 @@
-import {model, Schema} from "mongoose";
+import { model, Schema } from 'mongoose';
+import moment from 'moment';
 
 const orderSchema = new Schema({
-    id: {type: String, required: true},
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-    image: {type: String, required: true},
-    email: {type: String, required: true}
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    email: { type: String, required: true },
+    createdAt: { type: String, default: moment().format('YYYY-MM-DD HH:mm') } // Format the date here
+});
 
+orderSchema.pre('save', function (next) {
+    // Format the current date/time before saving
+    this.createdAt = moment().format('YYYY-MM-DD HH:mm');
+    next();
 });
 
 export const OrderModel = model('orders', orderSchema);
