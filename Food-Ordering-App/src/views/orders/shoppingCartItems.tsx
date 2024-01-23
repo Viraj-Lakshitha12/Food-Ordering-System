05585e0c630d3ddf662563b48500b934181bd9f0
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-23 09:49:44 +0530

    update checlout

--- Food-Ordering-App/src/views/orders/shoppingCartItems.tsx
+++ Food-Ordering-App/src/views/orders/shoppingCartItems.tsx
@@ -2,7 +2,7 @@ import {useCart} from '../dashboard/menu/CartContext.tsx';
 import Delete from '../../assets/icons/delete.tsx';
 import {Bounce, toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import  {ChangeEvent, useEffect, useState} from "react";
+import {ChangeEvent, useState} from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 
@@ -28,28 +28,25 @@ export default function ShoppingCartItems() {
         setUserData((prevData) => ({...prevData, [type]: e.target.value}));
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                let userEmail = Cookies.get('user');
-                const response = await axios.get(`http://localhost:8080/api/user/getUserDetailsByEmail/${userEmail}`);
-                const userDetailData = response.data.data;
 
-                // Set user details in the state
-                setUserData({
-                    userName: userDetailData.userName,
-                    email: userDetailData.email,
-                    address: userDetailData.address,
-                    postalCode: userDetailData.postalCode,
-                    city: userDetailData.city,
-                });
-            } catch (error) {
-                console.error("Error fetching user details:", error);
-            }
-        };
+    const fetchData = async () => {
+        try {
+            let userEmail = Cookies.get('user');
+            const response = await axios.get(`http://localhost:8080/api/user/getUserDetailsByEmail/${userEmail}`);
+            const userDetailData = response.data.data;
 
-        fetchData();
-    }, []);
+            // Set user details in the state
+            setUserData({
+                userName: userDetailData.userName,
+                email: userDetailData.email,
+                address: userDetailData.address,
+                postalCode: userDetailData.postalCode,
+                city: userDetailData.city,
+            });
+        } catch (error) {
+            console.error("Error fetching user details:", error);
+        }
+    };
 
 
     const notify = () => {
@@ -63,13 +60,15 @@ export default function ShoppingCartItems() {
 
     // Calculate total price
     const total: number = cartItems.reduce((acc, item) => acc + item.price, 0);
-
+    {
+        total > 0 ? fetchData() : null
+    }
     return (
         <section className="mt-8 ml-8 mr-8">
             <div className="text-center text-red-600 mb-8 font-bold text-3xl">
                 <h3>Cart</h3>
             </div>
-            <div className="gap-4 grid grid-cols-2 pb-8 border-b">
+            <div className="gap-16 grid grid-cols-2 pb-8 border-b">
                 <div>
                     {cartItems?.length === 0 ? 'No Product item Available' : cartItems?.map((item) => (
                         <div className="flex gap-16 mt-4 pb-2 items-center border-b" key={item.id}>
@@ -99,64 +98,66 @@ export default function ShoppingCartItems() {
                         Total: ${total.toFixed(2)}
                     </div>
                 </div>
-                <div className={' p-8 bg-gray-200 rounded-xl'}>
-                    <h3 className={'text-xl font-semibold text-center'}>Checkout</h3>
-                    <form>
-                        <div className="grid items-center">
-                            <div className="my-2 grid grid-cols-2 gap-4">
+                <div className={' p-8 bg-gray-100 rounded-xl'}>
+                    <h3 className={'text-2xl text-red-600 font-bold text-center'}>Checkout</h3>
 
-                                <input
-                                    id="firstName"
-                                    className="col-span-2 p-2 my-1 max-w-4xl mx-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
-                                    type="text"
-                                    placeholder="First and last name"
-                                    value={userData.userName}
-                                    onChange={(e) => handleInput(e, 'userName')}
-                                />
-                                <input
-                                    id="email"
-                                    className="col-span-2 p-2 max-w-4xl mx-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
-                                    type="text"
-                                    placeholder="example@gmail.com"
-                                    value={userData.email}
-                                    onChange={(e) => handleInput(e, 'email')}
-                                    disabled
-                                />
+                    <form className="flex flex-col items-center">
+                        <div className="my-2 ml-10 grid gap-5 min-w-full items-center">
+                            <input
+                                id="firstName"
+                                className="col-span-2 p-2 my-1 max-w-xl mx-2 border-gray-400 border-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
+                                type="text"
+                                placeholder="First and last name"
+                                value={userData.userName}
+                                onChange={(e) => handleInput(e, 'userName')}
+                            />
+                            <input
+                                id="email"
+                                className="col-span-2 p-2 max-w-xl mx-2 border-solid border-gray-400 border-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
+                                type="text"
+                                placeholder="example@gmail.com"
+                                value={userData.email}
+                                onChange={(e) => handleInput(e, 'email')}
+                                disabled
+                            />
+                            <input
+                                id="address"
+                                className="col-span-2 p-2 max-w-xl mx-2 rounded-md bg-gray-200 border-gray-400 border-2 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
+                                type="text"
+                                placeholder="street address"
+                                value={userData.address}
+                                onChange={(e) => handleInput(e, 'address')}
+                            />
+                        </div>
+                        <div className="my-1 mt-2 grid grid-cols-2 gap-5 mr-12">
+                            <input
+                                id="postalCode"
+                                className="mt-1 p-2 max-w-sm  rounded-md bg-gray-200 text-black border-gray-400 border-2 font-semibold hover:border-2 hover:border-blue-800 text-center"
+                                type="text"
+                                placeholder="postal code"
+                                value={userData.postalCode}
+                                onChange={(e) => handleInput(e, 'postalCode')}
+                            />
+                            <input
+                                id="city"
+                                className="mt-1 p-2 max-w-sm rounded-md bg-gray-200 text-black border-gray-400 border-2 font-semibold hover:border-2 hover:border-blue-800 text-center"
+                                type="text"
+                                placeholder="city"
+                                value={userData.city}
+                                onChange={(e) => handleInput(e, 'city')}
+                            />
+                        </div>
+                        <div className={'mt-4 items-center max-w-full'}>
+                            <button type="submit"
+                                    className="text-white bg-red-600 rounded-md py-2 mr-8 px-24 text-xl">
+                                Pay ${total}
+                            </button>
 
-                                <input
-                                    id="address"
-                                    className="col-span-2 p-2 max-w-4xl mx-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
-                                    type="text"
-                                    placeholder="street address"
-                                    value={userData.address}
-                                    onChange={(e) => handleInput(e, 'address')}
-                                />
-                            </div>
-                            <div className="my-1 grid grid-cols-2 gap-4">
-                                <input
-                                    id="postalCode"
-                                    className="mt-1 p-2 max-w-4xl mx-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
-                                    type="text"
-                                    placeholder="postal code"
-                                    value={userData.postalCode}
-                                    onChange={(e) => handleInput(e, 'postalCode')}
-                                />
-                                <input
-                                    id="city"
-                                    className="mt-1 p-2 max-w-4xl mx-2 rounded-md bg-gray-200 text-black font-semibold hover:border-2 hover:border-blue-800 text-center"
-                                    type="text"
-                                    placeholder="city"
-                                    value={userData.city}
-                                    onChange={(e) => handleInput(e, 'city')}
-                                />
-                            </div>
                         </div>
 
-
-                        <button type={'submit'}
-                                className={'w-full text-white bg-red-600 rounded-md py-4 px-24 text-xl'}>Pay
-                            ${total}</button>
                     </form>
+
+
                 </div>
             </div>
             <ToastContainer
