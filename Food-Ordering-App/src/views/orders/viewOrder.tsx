43c063cc17419941d59da7357b591a27cfb760b4
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-25 08:42:01 +0530

    create vew order page

--- Food-Ordering-App/src/views/orders/viewOrder.tsx
+++ Food-Ordering-App/src/views/orders/viewOrder.tsx
@@ -1,7 +1,8 @@
 export default function ViewOrder() {
     return (
-        <section>
-            View orders
+        <section className={'my-16'}>
+            <div className={'text-4xl text-center font-semibold text-red-600 italic'}>Your Order</div>
+            <div className={'text-center font-semibold text-xl italic'}>Thanks for your order.</div>
         </section>
     );
 }
