35f06fadacc00596998058f3149e889483e1ae2e
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-25 09:28:08 +0530

    update order details model

--- Food-Ordering-backend/src/models/orderDetails.ts
+++ Food-Ordering-backend/src/models/orderDetails.ts
@@ -1,25 +1,23 @@
-import {model, Schema} from "mongoose";
+import { model, Schema, Types } from "mongoose";
 import moment from "moment/moment";
 
 const orderDetailsSchema = new Schema({
-    items: [{ // Assuming items is an array of items
-        id: {type: String, required: true},
-        name: {type: String, required: true},
-        description: {type: String, required: true},
-        image: {type: String, required: true},
-        price: {type: Number, required: true}
+    order: { type: Types.ObjectId, ref: 'order', required: true }, // Reference to Order
+    items: [{
+        id: { type: String, required: true },
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true }
     }],
-    total: {type: Number, required: true}, // Add the total field
-    email: {type: String, required: true},
-    userName: {type: String, required: true},
-    address: {type: String, required: true},
-    postalCode: {type: String, required: true},
-    city: {type: String, required: true},
-    createdAt: {type: String, default: moment().format('YYYY-MM-DD HH:mm')}
-
-
+    total: { type: Number, required: true },
+    email: { type: String, required: true },
+    userName: { type: String, required: true },
+    address: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    city: { type: String, required: true },
+    createdAt: { type: String, default: moment().format('YYYY-MM-DD HH:mm') }
 });
-
 
 orderDetailsSchema.pre('save', function (next) {
     // Format the current date/time before saving
