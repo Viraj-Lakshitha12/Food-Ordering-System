7beacdb717aca990f562fce2393f17ebdf9d8af3
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-01-28 19:58:10 +0530

    update content

--- Food-Ordering-App/src/layout/hero.tsx
+++ Food-Ordering-App/src/layout/hero.tsx
@@ -2,6 +2,11 @@ import Right from "../assets/icons/right.tsx";
 
 
 export default function Hero() {
+
+    function handleOrderNowButton() {
+
+    }
+
     return (
         <section className={'grid sm:grid-cols-1 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 p-14 leading-10'}>
             <div className={'p-8 mt-[4vh] '}>
@@ -13,7 +18,9 @@ export default function Hero() {
                     Pizza is a missing piece that make everyday complete a simple yet delicious joy in life.
                 </p>
                 <div className={'flex gap-4 mt-[5vh] font-bold'}>
-                    <button className={'bg-red-500 text-white rounded-full p-8 py-3 flex gap-2 items-center'}>
+                    <button
+                    onClick={handleOrderNowButton}
+                        className={'bg-red-500 text-white rounded-full p-8 py-3 flex gap-2 items-center'}>
                         ORDER NOW
                         <Right/>
                     </button>
